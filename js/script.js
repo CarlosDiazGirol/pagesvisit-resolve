9daec0c62e5734688ad3943fa86d6f2b93948f14
Carlos <carlosdiazgirol@hotmail.com>
2023-11-14 11:23:04 +0100

    resolve

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const contadorVisitasSpan = document.getElementById('contadorVisitas');
+const btnReestablecer = document.getElementById('btnReestablecer')
+
+// Obtener el contador de visitas del localStorage
+const obtenerContadorLocalStorage = () => parseInt(localStorage.getItem('contadorVisitas')) || 0;
+let contadorVisitas = obtenerContadorLocalStorage();
+const guardarContadorLocalStorage = (contador) => localStorage.setItem('contadorVisitas', contador);
+
+const actualizarContador = () => {
+    contadorVisitasSpan.textContent = contadorVisitas;
+    // Guardar el contador actualizado en el localStorage
+    guardarContadorLocalStorage(contadorVisitas);
+}
+
+// Incrementar el contador y actualizar la página
+contadorVisitas++;
+actualizarContador();
+
+btnReestablecer.addEventListener("click", () => {
+  localStorage.setItem('contadorVisitas', 0)
+  contadorVisitasSpan.textContent = localStorage.getItem('contadorVisitas');
+});
+
+
